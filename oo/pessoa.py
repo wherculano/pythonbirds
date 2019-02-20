f7b2dafce28e16d3ce027d439e3f960880f4047b
wherculano <wagherculano@hotmail.com>
2019-02-19 22:15:46 -0300

    Criado atributo complexo "filhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=30):
+    def __init__(self, *filhos, nome=None, idade=30):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Herculano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome ='Wagner'
-    print(p.nome)
-    print(p.idade)
+    wagner = Pessoa(nome='Wagner')
+    herculano = Pessoa(wagner, nome='Herculano')
+    print(Pessoa.cumprimentar(herculano))
+    print(id(herculano))
+    print(herculano.cumprimentar())
+    print(herculano.nome)
+    print(herculano.idade)
+    for filho in herculano.filhos:
+        print(filho.nome)
